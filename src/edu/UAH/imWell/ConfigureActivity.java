46a403d1170f3c34364d3fbe726bd8597d6343e9
Tshrove <Tshrove@db357f82-f988-fba2-8270-d55cb6b3b1ef>
2012-02-14 01:22:28 +0000

    Add the configure activity in.

---
+++ src/edu/UAH/imWell/ConfigureActivity.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package edu.UAH.imWell;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConfigureActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configure);
+    }
+}
