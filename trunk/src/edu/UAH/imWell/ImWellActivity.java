5e49fe0a47b6a24089e70ac4833b2f4fc51c30ab
Tshrove <Tshrove@db357f82-f988-fba2-8270-d55cb6b3b1ef>
2012-02-14 01:22:28 +0000

    Add the configure activity in.

--- trunk/src/edu/UAH/imWell/ImWellActivity.java
+++ trunk/src/edu/UAH/imWell/ImWellActivity.java
@@ -1,13 +1,25 @@
 package edu.UAH.imWell;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ImWellActivity extends Activity {
+	Button btnConfigure;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        btnConfigure = (Button)findViewById(R.id.btnConfig);
+        btnConfigure.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent("edu.UAH.Configure"));				
+			}
+		});
     }
 }
